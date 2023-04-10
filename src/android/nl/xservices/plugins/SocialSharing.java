2648b19511153511e7a94c5439a02374de37d4e2
Raeesaa Metkari <raeesaa.metkari@excellarate.com>
2023-04-10 16:41:29 +0530

    minor fix

--- src/android/nl/xservices/plugins/SocialSharing.java
+++ src/android/nl/xservices/plugins/SocialSharing.java
@@ -657,6 +657,10 @@ public class SocialSharing extends CordovaPlugin {
           Intent messagingIntent= cordova.getContext().getPackageManager().getLaunchIntentForPackage(GOOGLE_SMS_APP);
           Boolean defaultAppSet = false;
 
+          if(defaultAppSet == false && messagingIntent != null) {
+            intent.setPackage(GOOGLE_SMS_APP);
+          }
+
           for (ResolveInfo info : resInfo) {
             String packageName = info.activityInfo.packageName.toLowerCase();
             if (packageName.contains("mms") || packageName.contains("messaging") || packageName.contains("message")
@@ -665,10 +669,6 @@ public class SocialSharing extends CordovaPlugin {
               intent.setPackage(info.activityInfo.packageName);
               break;
             }
-          }
-
-          if(defaultAppSet == false && messagingIntent != null) {
-            intent.setPackage(GOOGLE_SMS_APP);
           }
 
           cordova.startActivityForResult(plugin, intent, 0);
